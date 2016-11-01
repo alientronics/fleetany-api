178357455a2d69e63a16e8c5fa6c529a8fb23898
Rafael <rafaelpviegas@gmail.com>
2016-10-31 23:41:16 -0200

    Correção na tradução da blade de envio de emails

--- resources/views/mail-alert.blade.php
+++ resources/views/mail-alert.blade.php
@@ -1,8 +1,8 @@
-{{Lang::get("mails.FleetNumber")}}: {{$alarm->vehicle_fleet}}<br/>
-{{Lang::get("mails.VehiclePlate")}}: {{$alarm->vehicle_plate}}<br/>
-{{Lang::get("mails.Driver")}}: {{$alarm->vehicle_driver}}<br/>
-{{Lang::get("mails.TireNumber")}}: {{$alarm->tire_number}}<br/>
-{{Lang::get("mails.AlarmType")}}: {{$alarm->type}}<br/>
-{{Lang::get("mails.AlarmDescription")}}: {{$alarm->description}}<br/>
-{{Lang::get("mails.VehicleLocation")}}: <a href="http://maps.google.com/?q={{$alarm->vehicle_latitude}},{{$alarm->vehicle_longitude}}">Latitude: {{$alarm->vehicle_latitude}}, Longitude: {{$alarm->vehicle_longitude}}</a><br/>
+{{trans("mails.FleetNumber")}}: {{$alarm->vehicle_fleet}}<br/>
+{{trans("mails.VehiclePlate")}}: {{$alarm->vehicle_plate}}<br/>
+{{trans("mails.Driver")}}: {{$alarm->vehicle_driver}}<br/>
+{{trans("mails.TireNumber")}}: {{$alarm->tire_number}}<br/>
+{{trans("mails.AlarmType")}}: {{$alarm->type}}<br/>
+{{trans("mails.AlarmDescription")}}: {{$alarm->description}}<br/>
+{{trans("mails.VehicleLocation")}}: <a href="http://maps.google.com/?q={{$alarm->vehicle_latitude}},{{$alarm->vehicle_longitude}}">Latitude: {{$alarm->vehicle_latitude}}, Longitude: {{$alarm->vehicle_longitude}}</a><br/>
 {{url('/')}}/vehicle/{{$alarm->vehicle_id}}
